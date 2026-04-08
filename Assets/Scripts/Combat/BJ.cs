b1e6497858bf9bb77eba0904feeb213900304746
Nazaruk11 <nazarukm11@gmail.com>
2026-04-07 23:41:48 -0400

    combat stuff

--- Assets/Scripts/Combat/BJ.cs
+++ Assets/Scripts/Combat/BJ.cs
@@ -17,21 +17,21 @@ public class BJ : MonoBehaviour
     int numPlayerCards = 0;
     int numEnemyCards = 0;
 
-    //public void beginBJ()
-    //{
-    //    Debug.Log(playerCards[numPlayerCards]);
-    //    Debug.Log(cards.Length);
-    //    Debug.Log(getCard());
-    //    playerCards[numPlayerCards].GetComponent<UnityEngine.UI.Image>().sprite = cards[getCard()];
-    //    playerCards[numPlayerCards].SetActive(true);
-    //    numPlayerCards++;
-    //    enemyCards[numEnemyCards].GetComponent<UnityEngine.UI.Image>().sprite = cards[getCard()];
-    //    enemyCards[numEnemyCards].SetActive(true);
-    //    numEnemyCards++;
-    //    playerCards[numPlayerCards].GetComponent<UnityEngine.UI.Image>().sprite = cards[getCard()];
-    //    playerCards[numPlayerCards].SetActive(true);
-    //    numPlayerCards++;
-    //}
+    public void beginBJ()
+    {
+        Debug.Log(playerCards[numPlayerCards]);
+        Debug.Log(cards.Length);
+        Debug.Log(getCard());
+        playerCards[numPlayerCards].GetComponent<UnityEngine.UI.Image>().sprite = cards[getCard()];
+        playerCards[numPlayerCards].SetActive(true);
+        numPlayerCards++;
+        enemyCards[numEnemyCards].GetComponent<UnityEngine.UI.Image>().sprite = cards[getCard()];
+        enemyCards[numEnemyCards].SetActive(true);
+        numEnemyCards++;
+        playerCards[numPlayerCards].GetComponent<UnityEngine.UI.Image>().sprite = cards[getCard()];
+        playerCards[numPlayerCards].SetActive(true);
+        numPlayerCards++;
+    }
 
     public int getCard()
     {
